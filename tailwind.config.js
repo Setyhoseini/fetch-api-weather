b1a6040892846ad7ffb8de57a6ff4ed3de989500
Setyhoseini <setayeshsadathoseini@gmail.com>
2024-11-24 20:13:09 +0330

    styled header and footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,14 @@ module.exports = {
   content: ["./*.{html,js}"],
   theme: {
     extend: {},
+
+    colors: {
+      'starcoach-logo-text': 'hsla(164, 99%, 40%, 1)',
+      'main-text-color': 'hsla(209, 73%, 7%, 1)',
+      'gray-text': 'hsla(0, 0%, 70%, 1)',
+      'header-footer-border': 'hsla(0, 0%, 92%, 1)',
+      'card-border': 'hsla(0, 0%, 94%, 1)',
+    },
   },
   plugins: [],
 }
